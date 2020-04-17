5f3d52dafa72104d08975fe16990c4b7325b7a17
Israel Gonzalez <48233351+israelgonzalezb@users.noreply.github.com>
2020-04-16 22:25:03 -0700

    Building out exploration function for adventure project. Currently working on randomized depth-irst traversal

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -11,13 +11,13 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+#map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -27,8 +27,47 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
-traversal_path = []
 
+# For the current room, pick a random direction to move if that direction is "?"
+# For that room, for any direction that returns an error, mark that exit as 0
+# For that room, for the first direction that returns success, mark that exit as 1
+# Add that direction traveled to the traversal path
+# For the current room, mark the opposite direction as 1 because you just came in that way
+
+explorer_map = {}
+
+
+def explorer():
+    exits = player.current_room.get_exits()
+    room = player.current_room.id
+    exit_count = len(exits)
+    random_indexes = []
+
+    if room not in explorer_map:
+        explorer_map[room] = {}
+
+    for exit_direction in exits:
+        if exit_direction not in explorer_map[room]:
+            explorer_map[room][exit_direction] = "?"
+
+    while len(random_indexes) < exit_count:
+        random_index = random.randrange(0, exit_count)
+        if random_index not in random_indexes:
+            random_indexes.append(random_index)
+
+    for idx in random_indexes:
+        player.travel(exits[idx])
+        explorer_map[room][exits[idx]] = player.current_room.id
+        explorer()
+
+    print(explorer_map)
+
+    #direction = directions[random.randint(0,4)]
+
+
+explorer()
+
+traversal_path = ["n", "n", "n"]
 
 
 # TRAVERSAL TEST
@@ -41,11 +80,11 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
 
 
 #######
